583dc87f425e6f3e905ad6690d28834f3deb2199
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-19 21:05:37 -0400

    bbatsov's rubocop style guide

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -3,44 +3,39 @@ def start_of_word(word, len)
 end
 
 def shout(word)
-  word.upcase  unless word.split.length > 1
-  arr = word.split
-  arr.map! do |a|
-    if (a.length > 3) || (a == arr.first)
-      a.upcase
-    end
-  end
-  arr.join(" ")
+  word.upcase
 end
 
-def echo (word)
+def echo(word)
   word
 end
 
-def titleize (w, count=1)
+def word_not_little(word)
+  littles = %w( the over and )
+  !littles.include?(word)
+end
+
+def titleize(w)
   arr = w.split
-  return w.capitalize unless arr.size > 1
-  littles = %w{ the ddd over and }
   arr.map! do |a|
-    if !littles.include?(a)
+    if word_not_little(a)
       a.capitalize!
     else
       a
     end
   end
-  f = arr.first
-  arr[0] = f.capitalize
-  arr.join(" ")
+  arr.first.capitalize!
+  arr.join(' ')
 end
 
-def repeat(w, count=2)
+def repeat(w, count = 2)
   stuff = []
   (0...count).each do
     stuff << w
   end
-  stuff.join(" ")
+  stuff.join(' ')
 end
 
-def first_word s
-  s.split(" ").first
+def first_word(s)
+  s.split(' ').first
 end
